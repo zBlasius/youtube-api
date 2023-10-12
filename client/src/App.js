631b9be28f8885e7fc1167379b5c789badf81966
Gustavo Blasius <gustavo.blasius@clinicorp.com>
2023-10-12 11:44:14 -0300

    feat: react app and chakra components

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          CLIENT
+          <Button colorScheme='blue'>Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
