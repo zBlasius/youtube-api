67f6c9457994c20896492ec43d90ca098e702f0c
Gustavo Blasius <gustavo.blasius@clinicorp.com>
2023-10-17 00:31:06 -0300

    feat: get youtube api by localstorage

--- client/src/Pages/MainScreen/index.tsx
+++ client/src/Pages/MainScreen/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect} from 'react';
 import C_Input from '../../Components/Input';
 import { Grid, GridItem } from '@chakra-ui/react'
 import C_Button from '../../Components/Button';
@@ -11,21 +11,17 @@ export default function MainScreen() {
     const context = useContext(Context);
     const { state, setState } = context;
     const [keyWord, setKeyWord] = useState("");
-    const [videoId, serVideoId] = useState(state.videoId);
+    const [videoId, setVideoId] = useState(state.videoId);
+    const [apiKey, setApiKey] = useState(localStorage.getItem('api_key') || '')
 
     function getListCommentsAtYoutubeApi(videoId: string) {
-        //setState({videoId, listComments:[{author: 'Sofia Coelho', comment:'Oloco meu'}]})
-        //SIm2W9TtzR0
-
-        axios.get(`https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoId}&key=AIzaSyBMmn56yDotiONwvbMe14unTMoIxhp1gPM`)
+        axios.get(`https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoId}&key=${apiKey}`)
             .then(ret => {
                 let newState: { author: any; comment: any; }[] = [];
                 let list = ret.data.items;
-                console.log('list', list)
                 list.forEach((item: any) => {
                     newState.push({author:item.snippet.topLevelComment.snippet.authorDisplayName ,comment:item.snippet.topLevelComment.snippet.textDisplay})
                 });
-
 
                 setState({videoId, listComments:newState})
             })
@@ -49,7 +45,13 @@ export default function MainScreen() {
             >
                 <GridItem w="100%" pl='2' bg='orange.400' area={'header'} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                     <div style={{ width: '50%', gap: 20 }}>
-                        <C_Input placeholder="videoId" value={videoId} onChange={(event) => serVideoId(event.target.value)} style={{ margin: '2px' }} />
+                        
+                        <C_Input placeholder="api_key" value={apiKey} onChange={(event) => {
+                            setApiKey(event.target.value);
+                            localStorage.setItem('api_key', event.target.value);
+                        }} style={{ margin: '2px' }} /> 
+
+                        <C_Input placeholder="videoId" value={videoId} onChange={(event) => setVideoId(event.target.value)} style={{ margin: '2px' }} />
                         <C_Input placeholder="key-word" value={keyWord} onChange={(event) => setKeyWord(event.target.value)} style={{ maring: '2px' }} />
                         <C_Button onClick={() => getListCommentsAtYoutubeApi(videoId)} />
                     </div>
